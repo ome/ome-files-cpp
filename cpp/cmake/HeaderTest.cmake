e0f446173dddf9c6d9edcfc07e9067d3c028db9c
Roger Leigh <r.leigh@dundee.ac.uk>
2013-12-18 16:10:55 +0000

    cmake: Add and use HeaderTest functions for header testing
    Each component computes its header set when running cmake, and
    writes this out to a Headers.cmake file in the corresponding
    test directory.  This header list may then be sourced and used
    by the test scripts, avoiding the need for racy globbing of
    the source and binary directories (it's racy because the source
    generation may not have occured at the time the globbing is
    performed).  This makes header test generation race-free and
    robust--it will always match the header set exactly.

--- cpp/cmake/HeaderTest.cmake
+++ cpp/cmake/HeaderTest.cmake
@@ -34,17 +34,49 @@
 # policies, either expressed or implied, of any organization.
 # #L%
 
-include_directories(${GTEST_INCLUDE_DIR})
+cmake_policy(SET CMP0007 NEW)
 
-if(BUILD_TESTS)
-  set(test_main_sources main.cpp)
+# Dump headers
+function(header_include_list_write source_headers binary_headers source_prefix test_dir)
+  set(TEST_FILE ${test_dir}/Headers.cmake)
+  file(WRITE "${TEST_FILE}" "set(TEST_HEADERS\n")
 
-  add_library(testmain STATIC ${test_main_sources})
+  foreach(header ${${source_headers}})
+    file(APPEND "${TEST_FILE}" "    ${source_prefix}/${header}\n")
+  endforeach(header)
 
-  set(TEST_LIBS testmain ${GTEST_LIBRARIES} ${CMAKE_THREAD_LIBS_INIT})
-endif(BUILD_TESTS)
+  foreach(header ${${binary_headers}})
+    file(RELATIVE_PATH header ${PROJECT_BINARY_DIR}/cpp/lib ${header})
+    file(APPEND "${TEST_FILE}" "    ${header}\n")
+  endforeach(header)
 
-add_subdirectory(ome-compat)
-add_subdirectory(ome-xerces)
-add_subdirectory(ome-xml)
-add_subdirectory(ome-bioformats)
+  file(APPEND "${TEST_FILE}" ")\n")
+  file(APPEND "${TEST_FILE}" "list(SORT TEST_HEADERS)\n")
+endfunction(header_include_list_write)
+
+function(header_test_from_file component path)
+  include(${CMAKE_CURRENT_BINARY_DIR}/Headers.cmake)
+  set(headerdir ${PROJECT_BINARY_DIR}/cpp/test/${component}/headers)
+  file(MAKE_DIRECTORY ${headerdir})
+
+  foreach(header ${TEST_HEADERS})
+    string(REPLACE "/" "_" genheader ${header})
+    string(REPLACE "${PROJECT_SOURCE_DIR}/cpp/src/" "" include ${header})
+    string(REPLACE "${headerdir}/" "" include ${include})
+    string(REGEX REPLACE "\\.h$" ".cpp" genheader ${genheader})
+    string(REGEX REPLACE "[/.]" "_" safeheader ${include})
+    string(CONFIGURE "#include <@include@>
+
+#include <gtest/gtest.h>
+
+TEST(Header, ${safeheader})
+{
+}
+" src)
+    file(WRITE "${headerdir}/${genheader}" "${src}")
+    list(APPEND test_headers_SOURCES "${headerdir}/${genheader}")
+  endforeach(header)
+
+  add_executable(${component}-headers ${test_headers_SOURCES})
+  target_link_libraries(${component}-headers ${TEST_LIBS})
+endfunction(header_test_from_file)
