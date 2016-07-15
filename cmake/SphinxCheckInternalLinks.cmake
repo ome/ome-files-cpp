92dad0a2476f4c9658735332c13f78838f991324
Roger Leigh <rleigh@codelibre.net>
2016-07-15 18:07:17 +0000

    docs: Document and correct link checks

--- cmake/SphinxCheckInternalLinks.cmake
+++ cmake/SphinxCheckInternalLinks.cmake
@@ -1,3 +1,6 @@
+# Check "local" sphinx links, using files in the build tree and in the
+# install tree (other components)
+
 message(STATUS "Checking internal links")
 
 set(failed OFF)
@@ -5,6 +8,7 @@ set(failed OFF)
 file(STRINGS "${OUTPUT}" output)
 file(REMOVE "${OUTPUT}.new")
 foreach(line IN LISTS output)
+  # Parse the filename, link type, link and anchor from each line
   string(REGEX MATCH "^.*: \\[.*\\] .*" line_match "${line}")
   string(REGEX MATCH "^.*: \\[.*\\] .*#.*" anchor_match "${line}")
   if(line_match)
@@ -18,22 +22,34 @@ foreach(line IN LISTS output)
       unset(anchor)
     endif()
 
+    # Only check local links
     if(type STREQUAL "local")
+      # Compute path relative to our install path
       file(RELATIVE_PATH relpath "/" "/${SPHINX_INSTALL_PATH}/${link}")
 
+      # Check sphinx link in the install tree
       unset(linkfile)
       if(EXISTS "${EXTERNAL_REFERENCE}/${relpath}")
         set(linkfile "${EXTERNAL_REFERENCE}/${relpath}")
       endif()
+      # Check sphinx link in the build tree
       if(EXISTS "${INTERNAL_REFERENCE}/${link}")
         set(linkfile "${INTERNAL_REFERENCE}/${link}")
       endif()
+      # Check doxygen link in the install tree
+      if(EXISTS "${DOXYGEN_REFERENCE}/${link}")
+        set(linkfile "${DOXYGEN_REFERENCE}/${link}")
+      endif()
+      # Check doxygen link in the build tree
       if(DOXYGEN_REFERENCE AND DOXYGEN_INSTALL_PATH)
-        string(REGEX REPLACE "^${DOXYGEN_INSTALL_PATH}/" "" doxygen_path "${relpath}")
+        string(REGEX REPLACE "^${DOXYGEN_INSTALL_PATH}/(.*)" "\\1" doxygen_path "${relpath}")
         if(EXISTS "${DOXYGEN_REFERENCE}/${doxygen_path}")
           set(linkfile "${DOXYGEN_REFERENCE}/${doxygen_path}")
         endif()
       endif()
+      # Check link and any anchor.  If the link does not exist, or the
+      # anchor does not exist, set the link type to broken when
+      # rewriting the output file with our test results.
       if(linkfile)
         if(anchor)
           file(STRINGS "${linkfile}" anchor_matches REGEX "<a +class=\"anchor\" +id=\"${anchor}\"")
