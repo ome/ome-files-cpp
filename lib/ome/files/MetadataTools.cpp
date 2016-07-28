8c21c5efc39ed89cfb4c353737ec5376a840ce94
Roger Leigh <rleigh@codelibre.net>
2016-07-28 11:02:56 +0100

    MetadataTools: Check for both BinData and TiffData when setting MetadataOnly

--- lib/ome/files/MetadataTools.cpp
+++ lib/ome/files/MetadataTools.cpp
@@ -548,7 +548,8 @@ namespace ome
           try
             {
               OMEXMLMetadata& omexml(dynamic_cast<OMEXMLMetadata&>(store));
-              if (omexml.getTiffDataCount(s) == 0)
+              if (omexml.getTiffDataCount(s) == 0 ||
+                  omexml.getPixelsBinDataCount(s) == 0)
                 addMetadataOnly(omexml, s);
             }
           catch (const std::bad_cast&)
@@ -592,7 +593,8 @@ namespace ome
           try
             {
               OMEXMLMetadata& omexml(dynamic_cast<OMEXMLMetadata&>(store));
-              if (omexml.getTiffDataCount(s) == 0)
+              if (omexml.getTiffDataCount(s) == 0 ||
+                  omexml.getPixelsBinDataCount(s) == 0)
                 addMetadataOnly(omexml, s);
             }
           catch (const std::bad_cast&)
