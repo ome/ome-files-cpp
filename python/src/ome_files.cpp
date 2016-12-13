ff23196ba9b13d42d542dedc7caaf837f51394bf
simleo <simleo@crs4.it>
2016-12-13 16:25:41 +0000

    python wrapper: initial stub

---
+++ python/src/ome_files.cpp
@@ -0,0 +1,36 @@
+#include <Python.h>
+
+#include <string>
+
+#include <ome/compat/memory.h>
+#include <ome/files/FormatReader.h>
+#include <ome/files/in/OMETIFFReader.h>
+
+
+static PyObject *
+get_image_count(PyObject *self, PyObject *args) {
+    const char *filename;
+    ome::compat::shared_ptr<ome::files::FormatReader> reader;
+    PyObject* image_count;
+    if (!PyArg_ParseTuple(args, "s", &filename)) {
+        return NULL;
+    }
+    reader = ome::compat::make_shared<ome::files::in::OMETIFFReader>();
+    reader->setId(std::string(filename));
+    image_count = PyInt_FromSize_t(reader->getImageCount());
+    reader->close();
+    return image_count;
+}
+
+
+static PyMethodDef OMEFilesMethods[] = {
+  {"get_image_count", get_image_count, METH_VARARGS,
+   "Open file and get image count"},
+  {NULL, NULL, 0, NULL} /* Sentinel */
+};
+
+
+PyMODINIT_FUNC
+initome_files(void) {
+  Py_InitModule3("ome_files", OMEFilesMethods, "OME Files wrapper");
+}
