38e218d8e42d25be635a1a9cc65d4b4fc1ea11ba
Roger Leigh <rleigh@dundee.ac.uk>
2017-07-12 10:50:59 +0100

    cmake: Use generic OME references for consistency

--- cmake/GTest.cmake
+++ cmake/GTest.cmake
@@ -1,5 +1,5 @@
 # #%L
-# OME Files C++ libraries (cmake build infrastructure)
+# OME C++ libraries (cmake build infrastructure)
 # %%
 # Copyright © 2006 - 2015 Open Microscopy Environment:
 #   - Massachusetts Institute of Technology
